c92fec1430a108c2f5d0536db23acc33d0ee0e7b
Nishant Siddh <94243692+nishant-siddh@users.noreply.github.com>
2023-03-19 02:00:58 +0530

    Add files via upload

--- src/Components/Data.jsx
+++ src/Components/Data.jsx
@@ -1,14 +1,23 @@
 import React from "react";
+import Rank from "./Rank";
+import Score2019 from "./Score2019";
+import Score2024 from "./Score2024";
+import Skill from "./Skill";
 
 const Data = ({ data }) => {
   return (
-    <div>
-      <>
-        {data.map((element) => {
-          return <div>{element.skill}</div>;
-        })}
-      </>
-    </div>
+    <>
+      <div className="flex gap-5 mt-5">
+        <div className="flex justify-around border-r-2 gap-5 w-3/5">
+          <Skill data={data} />
+          <Score2019 data={data} />
+          <Score2024 data={data} />
+        </div>
+        <div>
+          <Rank data={data} />
+        </div>
+      </div>
+    </>
   );
 };
 
